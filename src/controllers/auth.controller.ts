605396dc1a729fb85b27fdae968aba6e85fa6cfd
yauhen <yauhentim@gmail.com>
2024-11-30 19:12:38 +0100

    add login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,27 +1,11 @@
 import catchErrors from "../utils/catchErrors";
-import { CREATED } from "../constants/http";
-import { z } from "zod";
-import { createAccount } from "../services/auth.service";
+import { CREATED, OK } from "../constants/http";
+import { createAccount, loginUser } from "../services/auth.service";
 import { setAuthCookies } from "../utils/cookies";
-
-const registerSchema = z
-  .object({
-    email: z.string().email().min(1).max(255),
-    password: z.string().min(6).max(255),
-    name: z.string().min(2).max(255),
-    surname: z.string().min(2).max(255),
-    confirmedPassword: z.string().min(6).max(255),
-    userAgent: z.string().optional(),
-  })
-  .refine((data) => data.password === data.confirmedPassword, {
-    message: "Passwords do not match",
-    path: ["confirmedPassword"],
-  });
+import { loginSchema, registerSchema } from "./auth.schemas";
 
 export const registerHandler = catchErrors(async (req, res) => {
-  const { email, password, name, surname, confirmedPassword } = req.body;
   // TODO: validate request body
-  console.log({ email, password, name, surname, confirmedPassword });
   const request = registerSchema.parse({
     ...req.body,
     userAgent: req.headers["user-agent"],
@@ -35,8 +19,25 @@ export const registerHandler = catchErrors(async (req, res) => {
     surname: request.surname,
     userAgent: request.userAgent,
   });
+
   //TODO: return response
   return setAuthCookies({ res, accessToken, refreshToken })
     .status(CREATED)
     .json(user);
+});
+
+export const loginHandler = catchErrors(async (req, res) => {
+  //TODO: validate request body
+  const request = loginSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+
+  //TODO: call service
+  const { accessToken, refreshToken } = await loginUser(request);
+
+  //TODO: return response
+  return setAuthCookies({ res, accessToken, refreshToken })
+    .status(OK)
+    .json({ message: "Login successful" });
 });
